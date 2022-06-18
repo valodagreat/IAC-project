e206c01f29c54c4b2c3dea09bccd9b2592b8be6e
valodagreat <vcaleb01@gmail.com>
2022-06-18 23:47:45 +0100

    Edited server

--- setup/files/index.js
+++ setup/files/index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const server = http.createServer(function (req, res) {
     res.writeHead(200);
-    res.end("Hello world oo!");
+    res.end("Hello world!");
 });
-server.listen(80);
+server.listen(3000);
